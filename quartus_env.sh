d81315419d1f4ffd65186babddbd6516042bd761
Rayan <rayandematos@gmail.com>
2024-10-16 09:45:45 -0300

    __git_main

--- quartus_env.sh
+++ quartus_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export QUARTUS_64BIT=1
+export QUARTUS_64BIT=1 # Remove if in a 32 bit system
 export ALTERA_ROOT="$HOME/altera/13.1" # Path where the Quartus is installed
 export QUARTUS_ROOTDIR_OVERRIDE="$ALTERA_ROOT/quartus"
 export QSYS_ROOTDIR="$QUARTUS_ROOTDIR_OVERRIDE/sopc_builder/bin"
